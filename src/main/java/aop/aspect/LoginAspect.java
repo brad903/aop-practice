6564264a7ae0cf71e6d094cdf1b6c87b676bd62b
Brad Lee <brad903@naver.com>
2018-12-23 02:33:50 +0900

    aop step1 구현완료

--- src/main/java/aop/aspect/LoginAspect.java
+++ src/main/java/aop/aspect/LoginAspect.java
@@ -1,4 +1,34 @@
 package aop.aspect;
 
+import aop.security.HttpSessionUtils;
+import aop.security.UnAuthenticationException;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpSession;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+@Aspect
+@Component
 public class LoginAspect {
+    private static final Logger log = getLogger(LoginAspect.class);
+
+    @Pointcut("execution(* aop.question.ApiQuestionController.*(..)) && args(session,..)")
+    public void getSession(HttpSession session) {
+    }
+
+    @Around("getSession(session)")
+    public Object sessionCheck(ProceedingJoinPoint jp, HttpSession session) throws Throwable {
+        if(!HttpSessionUtils.isLogin(session)) {
+            log.info("====== aop 예외 발생 ======");
+            throw new UnAuthenticationException("로그인이 필요합니다.");
+        }
+        return jp.proceed();
+    }
+
 }
